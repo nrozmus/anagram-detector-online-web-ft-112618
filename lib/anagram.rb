2b20b902b2aacd59d62210910debd20b714d6c8b
Nicholas Rozmus <rozmusnicholas@gmail.com>
2018-12-07 18:39:20 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,21 @@
 # Your code goes here!
+require 'pry'
+
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(array)
+    result = []
+    array.each do |word_in_array|
+      if word_in_array.split("").sort == word.split("").sort
+        result << word_in_array
+      end
+    end
+    result
+  end
+
+end
