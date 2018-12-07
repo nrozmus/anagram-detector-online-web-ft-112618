76e723a261ea9c9af604abc54df0a7f3710dbe43
Nicholas Rozmus <rozmusnicholas@gmail.com>
2018-12-07 18:37:36 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(words)
+    anagram = []
+    words.each do |wordy_words|
+      if wordy_words.split("").sort == word.split("").sort
+        anagram << wordy_words
+      end
+    end
+    anagram
+  end
+end
